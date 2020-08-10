4ed5b8decfa275f58ca819b7281d1d25e0461987
cnachteg <charlottenachtegael@hotmail.com>
2020-08-10 09:54:58 +0200

    Add test files and templates files

---
+++ test/test_calcul.py
@@ -0,0 +1,33 @@
+import unittest
+import calcul
+
+l = [(3,4),(5.6,6),(1.2,2.3)]
+
+class TestCalculSimple(unittest.TestCase):
+    def test_add(self):
+        for a,b in l:
+            with self.subTest(a=a,b=b):
+                self.assertEqual(calcul.addition(a,b),a+b)
+
+    def test_sous(self):
+        for a, b in l:
+            with self.subTest(a=a, b=b):
+                self.assertEqual(calcul.soustraction(a, b), a - b)
+
+    def test_div(self):
+        for a, b in l:
+            with self.subTest(a=a, b=b):
+                self.assertEqual(calcul.division(a, b), a / b)
+
+    def test_mul(self):
+        for a, b in l:
+            with self.subTest(a=a, b=b):
+                self.assertEqual(calcul.multiplication(a, b), a * b)
+
+
+class TestCalculComb(unittest.TestCase):
+    def test_add_and_sub(self):
+        for a,b in l:
+            with self.subTest(a=a,b=b):
+                self.assertEqual(calcul.addition(calcul.soustraction(a,b),calcul.soustraction(b,a)), (a-b) + (b-a))
+
---
+++ test/test_hello.py
@@ -0,0 +1,6 @@
+import unittest
+import hello
+
+class TestHello(unittest.TestCase):
+    def test_string(self):
+        self.assertEqual(hello.hello('Charlotte'), "Hello Charlotte !", msg="Ooops, this is not the right message !")
